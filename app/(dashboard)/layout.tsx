d517c819ec47701e8cc59f44dee8b5682fdbb6b8
davidmakarovv <davidmakarr@gmail.com>
2024-07-05 22:24:51 +0400

    header changed

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+
+const DashboardLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
+
+export default DashboardLayout;
