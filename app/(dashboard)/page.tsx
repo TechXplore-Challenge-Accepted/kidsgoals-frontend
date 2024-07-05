6fb6521e6284a0c5d5f386bbe8e4f70ead626ff9
davidmakarovv <davidmakarr@gmail.com>
2024-07-05 22:34:47 +0400

    changed mainpage h1

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className="w-full min-h-screen bg-[#FFF5C2] py-36">
       <div className="w-full p-20 flex justify-between">
-        <h1>Home Page</h1>
+        <h1>This Is Home Page</h1>
       </div>
     </section>
   );
